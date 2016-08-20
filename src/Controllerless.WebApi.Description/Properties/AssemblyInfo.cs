0d83c648e9495af95013faf86f52540c834f04ce
dotnetjunkie <steven@cuttingedge.it>
2016-08-20 12:23:26 +0200

    Assembly info updated.

--- src/Controllerless.WebApi.Description/Properties/AssemblyInfo.cs
+++ src/Controllerless.WebApi.Description/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Controllerless.WebApi.Description")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SolidServices.Controllerless.WebApi.Description")]
+[assembly: AssemblyDescription("Custom ApiExplorer for genrating API documentation for message based apis.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dotnetjunkie")]
 [assembly: AssemblyProduct("Controllerless.WebApi.Description")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 dotnetjunkie")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
